c207bdcce52a0052654990e0288da1bf12b25db9
rasa ichimori <rasaichimori@gmail.com>
2023-08-07 17:56:14 -0600

    fir

---
+++ dozeculture.com/dozeculture/src/app/present/page.tsx
@@ -0,0 +1,7 @@
+export default function Present() {
+    return (
+        <div>
+            No shade to boring websites, but that's just not our thing.
+        </div>
+    )
+}
---
+++ dozeculture.com/dozeculture/src/components/noiseFilter.js
@@ -0,0 +1,15 @@
+export default function noiseFilter() {
+    return (
+        <svg viewBox='0 0 300 300' xmlns='http://www.w3.org/2000/svg'>
+            <filter id='noiseFilter'>
+                <feTurbulence
+                    type='fractalNoise'
+                    baseFrequency='2.06'
+                    numOctaves='3'
+                    stitchTiles='stitch' />
+            </filter>
+
+            <rect width='100%' height='100%' filter='url(#noiseFilter)' />
+        </svg>
+    )
+}
